88c7c9ffcf4cfa09dc80fb604e5b53dd4da8a971
McAlvaro <mc.alv641@gmail.com>
2024-04-16 08:28:01 -0400

    App init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <main>
+        <div class="flex flex-col items-center justify-center">
+            <RouterView />
+        </div>
+    </main>
+</template>
